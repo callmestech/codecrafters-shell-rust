4601261c95d7373680da0be7761e53e542fd4a14
Sergei Kharchikov <callmestech@gmail.com>
2024-12-07 23:34:37 +0000

    handle empty string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use core::fmt;
-#[allow(unused_imports)]
 use std::io::{self, Write};
 use std::{
     fmt::Display,
@@ -31,7 +30,7 @@ impl Display for Command {
             Command::Echo => write!(f, "echo"),
             Command::Invalid(command) => {
                 let cmd = command.to_string();
-                write!(f, "{}", format!("{}: not found", cmd))
+                write!(f, "{}: not found", cmd)
             }
             Command::Type => write!(f, "type"),
         }
@@ -43,32 +42,37 @@ fn main() {
     let mut stdout = io::stdout();
     print!("$ ");
     stdout.flush().unwrap();
-
     // Wait for user input
     loop {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
         let parsed_input: Vec<&str> = input.split_whitespace().collect();
-        let command: Command = parsed_input[0].into();
-        match command {
-            Command::Exit => process::exit(0),
-            Command::Echo => {
-                if parsed_input.len() > 1 {
-                    println!("{}", parsed_input[1..].join(" "));
-                }
-            }
-            Command::Type => {
-                let command_to_describe: Command = parsed_input[1].into();
-                let description = match command_to_describe {
-                    Command::Exit | Command::Echo | Command::Type => {
-                        format!("{} is a shell builtin", command_to_describe)
+        let cmd: Option<Command> = parsed_input.first().map(|cmd| Command::from(*cmd));
+
+        if let Some(command) = cmd {
+            match command {
+                Command::Exit => process::exit(0),
+                Command::Echo => {
+                    if parsed_input.len() > 1 {
+                        println!("{}", parsed_input[1..].join(" "));
                     }
-                    command => command.to_string(),
-                };
-                println!("{}", description);
+                }
+                Command::Type => {
+                    let command_to_describe: Command = parsed_input[1].into();
+                    let description = match command_to_describe {
+                        Command::Exit | Command::Echo | Command::Type => {
+                            format!("{} is a shell builtin", command_to_describe)
+                        }
+                        command => command.to_string(),
+                    };
+                    println!("{}", description);
+                }
+                command => println!("{}", command),
             }
-            command => println!("{}", command),
         }
         print!("$ ");
         stdout.flush().unwrap();
