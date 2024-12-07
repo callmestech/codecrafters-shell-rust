cba3415de03fe78082cc6c3f4fed438018e449f4
Sergei Kharchikov <callmestech@gmail.com>
2024-12-07 13:21:55 +0000

    handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ fn main() {
     io::stdout().flush().unwrap();
 
     // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+        match input {
+            "exit" => break,
+            _ => println!("{}: command not found", input),
+        }
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
 }
