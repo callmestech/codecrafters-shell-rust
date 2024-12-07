80cf03b60fabcb5c11c638785ee82691a517a529
Sergei Kharchikov <callmestech@gmail.com>
2024-12-07 13:39:52 +0000

    implement echo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process;
+use std::process::{self};
+
+enum Command {
+    Exit,
+    Echo,
+    Invalid,
+}
+
+impl From<&str> for Command {
+    fn from(s: &str) -> Self {
+        match s {
+            "exit" => Command::Exit,
+            "echo" => Command::Echo,
+            _ => Command::Invalid,
+        }
+    }
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -13,9 +29,16 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
-        match input {
-            "exit 0" => process::exit(0),
-            _ => println!("{}: command not found", input),
+        let parsed_input: Vec<&str> = input.split_whitespace().collect();
+        let command = Command::from(parsed_input[0]);
+        match command {
+            Command::Exit => process::exit(0),
+            Command::Echo => {
+                if parsed_input.len() > 1 {
+                    println!("{}", parsed_input[1..].join(" "));
+                }
+            }
+            Command::Invalid => println!("{}: command not found", input),
         }
         print!("$ ");
         stdout.flush().unwrap();
