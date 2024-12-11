2b13d4b58831dd0d3e0498c51ebda225bfaf1fb5
Sergei Kharchikov <callmestech@gmail.com>
2024-12-11 00:53:26 +0000

    backslash outside quotes

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ use std::{collections::HashSet, io};
 pub mod algebra;
 pub mod command;
 
+const BACKSLASH: char = '\\';
+const SINGLE_QUOTE: char = '\'';
+const DOUBLE_QUOTE: char = '"';
+const SPACE: char = ' ';
+
 /// Find a command in the PATH environment variable
 /// Return the path of the command if found
 pub fn find_cmd_in_path(cmd: &str, path: &[String]) -> Option<String> {
@@ -36,54 +41,69 @@ pub fn read_path_env() -> Vec<String> {
 pub fn parse_input(input: &str) -> Vec<String> {
     let (mut args, _, arg_acc) = input.chars().fold(
         (vec![], HashSet::new(), vec![]),
-        |(mut args, mut quotes_set, mut arg_acc), char| {
-            // The double quotes are opening
-            if char == '"' && !quotes_set.contains(&char) {
-                quotes_set.insert(char);
-                (args, quotes_set, arg_acc)
+        |(mut args, mut special_symbols_set, mut arg_acc), char| {
+            match char {
+                // The double quotes are opening
+                DOUBLE_QUOTE if !special_symbols_set.contains(&char) => {
+                    special_symbols_set.insert(char);
+                }
                 // The double quotes are closing
                 // So we push the word between quotes to args and reset the arg_acc
-            } else if char == '"' && quotes_set.contains(&char) {
-                quotes_set.remove(&char);
-                if !arg_acc.is_empty() {
-                    args.push(arg_acc.into_iter().collect::<String>());
-                    arg_acc = vec![];
+                DOUBLE_QUOTE if special_symbols_set.contains(&char) => {
+                    special_symbols_set.remove(&char);
+                    if !arg_acc.is_empty() {
+                        args.push(arg_acc.into_iter().collect::<String>());
+                        arg_acc = vec![];
+                    }
                 }
-                (args, quotes_set, arg_acc)
-            } else if char == '\'' && !quotes_set.contains(&char) {
-                // it means we encountered a single quote
-                // inside of the opened double quotes
-                // so we add it to the arg_acc
-                if quotes_set.contains(&'"') {
-                    arg_acc.push(char);
-                } else {
-                    // The single quotes are opening
-                    quotes_set.insert(char);
+                SINGLE_QUOTE if !special_symbols_set.contains(&char) => {
+                    // it means we encountered a single quote
+                    // inside of the opened double quotes
+                    // so we add it to the arg_acc
+                    if special_symbols_set.contains(&DOUBLE_QUOTE) {
+                        arg_acc.push(char);
+                    } else {
+                        // The single quotes are opening
+                        special_symbols_set.insert(char);
+                    }
                 }
-                (args, quotes_set, arg_acc)
                 // The single quotes are closing
                 // So we push the word between quotes to args and reset the arg_acc
-            } else if char == '\'' && quotes_set.contains(&char) {
-                quotes_set.remove(&char);
-                if !arg_acc.is_empty() {
-                    args.push(arg_acc.into_iter().collect::<String>());
-                    arg_acc = vec![];
+                SINGLE_QUOTE if special_symbols_set.contains(&char) => {
+                    special_symbols_set.remove(&char);
+                    if !arg_acc.is_empty() {
+                        args.push(arg_acc.into_iter().collect::<String>());
+                        arg_acc = vec![];
+                    }
                 }
-                (args, quotes_set, arg_acc)
                 // If we have a space and the quotes_stack is empty
                 // We don't need to add the space to the arg_acc
-            } else if char == ' ' && quotes_set.is_empty() {
-                // If the arg_acc is not empty we add it to the args
-                // and reset the arg_acc
-                if !arg_acc.is_empty() {
-                    args.push(arg_acc.into_iter().collect());
-                    arg_acc = vec![];
+                SPACE if special_symbols_set.is_empty() => {
+                    // If the arg_acc is not empty we add it to the args
+                    // and reset the arg_acc
+                    if !arg_acc.is_empty() {
+                        args.push(arg_acc.into_iter().collect());
+                        arg_acc = vec![];
+                    }
                 }
-                (args, quotes_set, arg_acc)
-            } else {
-                arg_acc.push(char);
-                (args, quotes_set, arg_acc)
+                SPACE if !special_symbols_set.is_empty() => {
+                    // If we have a space and the special_symbols_set is not empty
+                    // we add the space to the arg_acc
+                    // and remove the backslash from the special_symbols_set
+                    if special_symbols_set.contains(&BACKSLASH) {
+                        special_symbols_set.remove(&BACKSLASH);
+                    }
+                    arg_acc.push(char);
+                }
+                BACKSLASH if special_symbols_set.is_empty() => {
+                    special_symbols_set.insert(char);
+                }
+                _ => {
+                    arg_acc.push(char);
+                }
             }
+
+            (args, special_symbols_set, arg_acc)
         },
     );
     // If the arg_acc is not empty we add it to the args
@@ -170,6 +190,20 @@ mod tests {
     fn test_parse_input_with_with_single_and_double_quotes() {
         let input = r#"echo 'bar'  "shell's"  'foo'"#;
         let expected = vec!["echo", "bar", "shell's", "foo"];
+        assert_eq!(parse_input(input), expected);
+    }
+
+    #[test]
+    fn test_parse_input_with_a_backslash_inside_of_double_quotes() {
+        let input = r#"echo "before\   after""#;
+        let expected = vec!["echo", r#"before\   after"#];
+        assert_eq!(parse_input(input), expected);
+    }
+
+    #[test]
+    fn test_parse_input_with_multiples_backslashes() {
+        let input = r#" echo hello\ \ \ \ \ \ shell"#;
+        let expected = vec!["echo", "hello      shell"];
         assert_eq!(parse_input(input), expected);
     }
 }
